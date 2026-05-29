7ccfa59d6a197b79cef73a7bb7f6c6c099f7f012
Hitesh <hitesh.d@simplior.com>
2026-05-29 17:58:37 +0530

    Checkout process

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
  *
  * @var bool
  */
-gghghhreer define( 'WP_USE_THEMES', true );
+Hitesh Checkout define( 'WP_USE_THEMES', true );
 
 /** Loads the WordPress Environment and Template */
 require __DIR__ . '/wp-blog-header.php';
